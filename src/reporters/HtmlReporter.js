8df0e3d01d1d52c4caff61829af66ec376672311
Tommy Montgomery <tommy.mont@gmail.com>
2011-05-27 01:32:37 -0700

    added overflow-x: auto to pre

--- src/reporters/HtmlReporter.js
+++ src/reporters/HtmlReporter.js
@@ -84,6 +84,7 @@
 	font-family: Consolas, Inconsolata, \"Courier New\", monospace;\
 	color: #000000;\
 	padding: 5px;\
+	overflow-x: auto;\
 	background-color: #FFFFFF;\
 	box-shadow: 0 15px 10px -10px rgba(0, 0, 0, 0.5), 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;\
 	-moz-box-shadow: 0 15px 10px -10px rgba(0, 0, 0, 0.5), 0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) inset;\
