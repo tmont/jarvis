4be5a68d6ee032ba94f64638a81d9cca10a9aa9e
Tommy Montgomery <tommy.mont@gmail.com>
2011-08-22 13:03:36 -0700

    run tests using node

---
+++ tests/test-node.js
@@ -0,0 +1,6 @@
+require("../src/bootstrap/jarvis.bootstrap.node.js");
+require("./ConstraintTests.js");
+require("./ExpectedErrorTests.js");
+require("./FailureMessageTests.js");
+require("./SampleTests.js");
+require("./SetupAndTearDownTests.js");
