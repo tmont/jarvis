adfc7d44c056f0e74094666964fa7f524ffa2d55
Tommy Montgomery <tommy.mont@gmail.com>
2011-08-22 18:04:09 -0700

    got tests working with the release version of node

--- src/reporters/CliReporter.js
+++ src/reporters/CliReporter.js
@@ -1,53 +1,52 @@
-(function(jarvis, undefined){
-	if (!console) {
-		throw "A console is required to report to the CLI";
-	}
+if (typeof(global.Jarvis) === "undefined") {
+	throw "Expected global variable \"Jarvis\" to be set";
+}
 
-	jarvis.Framework.Reporters.CliReporter = function() {
-		var tests = {}, indent = "";
+Jarvis.Framework.Reporters.CliReporter = function() {
+	var tests = {}, indent = "";
 
-		this.summary = function(totalAssertions) {
-			//not implemented
-		};
-
-		this.startTest = function(name, id) {
-			var test = {
-				name: name,
-				startTime: new Date().getTime()
-			};
-
-			tests[id] = test;
-
-			console.log(indent + test.name);
-			indent += "  ";
-		};
-
-		this.endTest = function(result, id) {
-			var endTime = new Date().getTime(),
-				test = tests[id];
-
-			switch (result.status) {
-				case "fail":
-				case "error":
-					console.error(indent + (result.message || ""));
-					console.log();
-					for (var i = 0; i < result.stackTrace.length; i++) {
-						console.error(indent + (i + 1) + ". " + result.stackTrace[i]);
-					}
-					break;
-				case "ignore":
-					console.warn(indent + (result.message || ""));
-					break;
-			}
-
-			console.log();
-			indent = indent.substring(0, indent.length - 2);
-			console.log(indent + (endTime - test.startTime) + "ms " + result.assertions + " assertion" + (result.assertions !== 1 ? "s" : ""));
-			console.log(indent + "-----------------------------------");
-			tests[id] = undefined;
-		};
+	this.summary = function(totalAssertions) {
+		//not implemented
 	};
 
-	jarvis.htmlDiffs = false;
-	jarvis.defaultReporter = new jarvis.Framework.Reporters.CliReporter();
-}(Jarvis));
+	this.startTest = function(name, id) {
+		var test = {
+			name: name,
+			startTime: new Date().getTime()
+		};
+
+		tests[id] = test;
+
+		console.log(indent + test.name);
+		indent += "  ";
+	};
+
+	this.endTest = function(result, id) {
+		var endTime = new Date().getTime(),
+			test = tests[id],
+			i;
+
+		switch (result.status) {
+			case "fail":
+			case "error":
+				console.error(indent + (result.message || ""));
+				console.log();
+				for (i = 0; i < result.stackTrace.length; i++) {
+					console.error(indent + (i + 1) + ". " + result.stackTrace[i]);
+				}
+				break;
+			case "ignore":
+				console.warn(indent + (result.message || ""));
+				break;
+		}
+
+		console.log();
+		indent = indent.substring(0, indent.length - 2);
+		console.log(indent + (endTime - test.startTime) + "ms " + result.assertions + " assertion" + (result.assertions !== 1 ? "s" : ""));
+		console.log(indent + "-----------------------------------");
+		tests[id] = undefined;
+	};
+};
+
+Jarvis.htmlDiffs = false;
+Jarvis.defaultReporter = new Jarvis.Framework.Reporters.CliReporter();
--- tests/test-node-release.js
+++ tests/test-node-release.js
@@ -1,9 +1,9 @@
-global.Jarvis = require("../src/jarvis.js");
-require("../src/reporters/CliReporter.js");
-
+global.Jarvis = require("../build/2.0/node/jarvis.js");
 global.Assert = Jarvis.Framework.Assert;
 global.Is = Jarvis.Framework.Is;
 global.Has = Jarvis.Framework.Has;
+
+require("../build/2.0/node/CliReporter.js");
 
 require("./ConstraintTests.js");
 require("./ExpectedErrorTests.js");
