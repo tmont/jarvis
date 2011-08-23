3538cd7baa728e62250ff35f1e2220c087aa7018
Tommy Montgomery <tommy.mont@gmail.com>
2011-08-22 18:08:29 -0700

    moved bootstrap stuff to root folder; fixed release test

--- src/bootstrap/jarvis.bootstrap.node.js
+++
@@ -1,6 +0,0 @@
-global.Jarvis = require("../../src/jarvis.js");
-require("../../src/reporters/CliReporter.js");
-
-global.Assert = Jarvis.Framework.Assert;
-global.Is = Jarvis.Framework.Is;
-global.Has = Jarvis.Framework.Has;
--- tests/test-node.js
+++ tests/test-node.js
@@ -1,9 +1,9 @@
 global.Jarvis = require("../src/jarvis.js");
-require("../src/reporters/CliReporter.js");
-
 global.Assert = Jarvis.Framework.Assert;
 global.Is = Jarvis.Framework.Is;
 global.Has = Jarvis.Framework.Has;
+
+require("../src/reporters/CliReporter.js");
 
 require("./ConstraintTests.js");
 require("./ExpectedErrorTests.js");
