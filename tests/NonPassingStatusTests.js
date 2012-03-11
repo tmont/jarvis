d4028624fbb7a82e1785c2ca8af459961bc4fdab
tmont <tommy.mont@gmail.com>
2012-03-10 22:58:29 -0800

    refactored non passing status tests

--- tests/NonPassingStatusTests.js
+++ tests/NonPassingStatusTests.js
@@ -1,45 +1,49 @@
-function Non_passing_status_tests() {
-	return [
+var suite = Jarvis.suite('Non passing status tests', {
+	tests: [
 		function Should_ignore_test() {
 			try {
 				Assert.ignore();
-				Assert.fail("Assert.ignore() did not throw a JarvisError");
+				Assert.fail('Assert.ignore() did not throw a JarvisError');
 			} catch (error) {
 				Assert.that(error.message, Is.empty());
-				Assert.that(error.type, Is.equalTo("ignore"));
+				Assert.that(error.type, Is.equalTo('ignore'));
 			}
 		},
-		
+
 		function Should_ignore_test_with_message() {
 			try {
-				Assert.ignore("ignoring");
-				Assert.fail("Assert.ignore() did not throw a JarvisError");
+				Assert.ignore('ignoring');
+				Assert.fail('Assert.ignore() did not throw a JarvisError');
 			} catch (error) {
-				Assert.that(error.message, Is.equalTo("ignoring"));
-				Assert.that(error.type, Is.equalTo("ignore"));
+				Assert.that(error.message, Is.equalTo('ignoring'));
+				Assert.that(error.type, Is.equalTo('ignore'));
 			}
 		},
-		
+
 		function Should_fail_test() {
 			try {
 				Assert.fail();
-				alert("Assert.fail() doesn't work");
+				alert('Assert.fail() doesn\'t work');
 			} catch (error) {
 				Assert.that(error.message, Is.empty());
-				Assert.that(error.type, Is.equalTo("fail"));
+				Assert.that(error.type, Is.equalTo('fail'));
 			}
 		},
-		
+
 		function Should_fail_test_with_message() {
 			try {
-				Assert.fail("failing");
-				alert("Assert.fail() doesn't work");
+				Assert.fail('failing');
+				alert('Assert.fail() doesn\'t work');
 			} catch (error) {
-				Assert.that(error.message, Is.equalTo("failing"));
-				Assert.that(error.type, Is.equalTo("fail"));
+				Assert.that(error.message, Is.equalTo('failing'));
+				Assert.that(error.type, Is.equalTo('fail'));
 			}
 		}
-	];
-}
+	]
+});
 
-Jarvis.run(Non_passing_status_tests);
+if (typeof(module) !== 'undefined') {
+	module.exports = suite;
+} else {
+	Jarvis.run(suite);
+}
