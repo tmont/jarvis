1270edc3b28ba9a95e8d39232912e38f67ac3bb6
tmont <tommy.mont@gmail.com>
2012-03-10 22:55:09 -0800

    refactored setup and teardown tests

--- tests/SetupAndTearDownTests.js
+++ tests/SetupAndTearDownTests.js
@@ -1,39 +1,41 @@
-function Setup_and_tear_down_tests() {
-	var foo = "original";
-	
-	return [
-		function Uses_bound_variable_without_setup_and_teardown1() {
-			Assert.that(foo, Is.equalTo("original"));
-		}, 
-		
-		{
-			setup: function() {
-				foo = "setup";
-			},
-			
-			tearDown: function() {
-				foo = "teardown";
-			},
-			
-			test: function Modifies_bound_variable_and_resets_it() {
-				return [
-					function Setup_and_tear_down_test1() {
-						Assert.that(foo, Is.equalTo("setup"));
-						foo = "something else";
-					},
-					
-					function Setup_and_tear_down_test2() {
-						Assert.that(foo, Is.equalTo("setup"));
-						foo = "something else again";
-					}
-				];
-			}
-		},
-		
-		function Uses_bound_variable_without_setup_and_teardown2() {
-			Assert.that(foo, Is.equalTo("teardown"));
-		}
-	];
-}
+var suite = Jarvis.suite('Setup and tear down tests', (function(){
+	var foo = 'original';
 
-Jarvis.run(Setup_and_tear_down_tests);
+	return {
+		tests: [
+			function Uses_bound_variable_without_setup_and_teardown1() {
+				Assert.that(foo, Is.equalTo('original'));
+			},
+
+			Jarvis.suite('Modified bound variable and resets it', {
+				setup: function() {
+					foo = 'setup';
+				},
+				tearDown: function() {
+					foo = 'teardown';
+				},
+				tests: [
+					function Setup_and_tear_down_test1() {
+						Assert.that(foo, Is.equalTo('setup'));
+						foo = 'something else';
+					},
+
+					function Setup_and_tear_down_test2() {
+						Assert.that(foo, Is.equalTo('setup'));
+						foo = 'something else again';
+					}
+				]
+			}),
+
+			function Uses_bound_variable_without_setup_and_teardown2() {
+				Assert.that(foo, Is.equalTo('teardown'));
+			}
+		]
+	};
+}()));
+
+if (typeof(module) !== 'undefined') {
+	module.exports = suite;
+} else {
+	Jarvis.run(suite);
+}
